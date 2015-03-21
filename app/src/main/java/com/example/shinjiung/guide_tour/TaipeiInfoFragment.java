092a6be5a418b4f26f47645f2e193fb82ed4dee7
JIUNG SHIN <jushin17@gmail.com>
2015-03-21 15:17:21 +0900

    taipei guide tour application

--- app/src/main/java/com/example/shinjiung/guide_tour/TaipeiInfoFragment.java
+++ app/src/main/java/com/example/shinjiung/guide_tour/TaipeiInfoFragment.java
@@ -1,13 +1,16 @@
 package com.example.shinjiung.guide_tour;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-import com.google.android.gms.maps.GoogleMap;
+
+
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
@@ -18,8 +21,33 @@ import com.google.android.gms.maps.model.MarkerOptions;
  */
 public class TaipeiInfoFragment extends Fragment {
     @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+
+        super.onActivityCreated(savedInstanceState);
+    }
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.taipei_map, container, false);
+        View view = inflater.inflate(R.layout.taipei_info, container, false);
+
+
+        WebView mWebView = (WebView) view.findViewById(R.id.taipei_web);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl("http://ko.m.wikipedia.org/wiki/타이페이_시");
+        mWebView.setWebViewClient(new WebClient());
+        mWebView.getSettings().setBuiltInZoomControls(true);
+        mWebView.getSettings().setSupportZoom(true);
+
+        return view;
+
     }
 
+
+    private class WebClient extends WebViewClient {
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+            return false;
+
+        }
+
+    }
 }
--- app/src/main/java/com/example/shinjiung/guide_tour/TaipeiMapFragment.java
+++ app/src/main/java/com/example/shinjiung/guide_tour/TaipeiMapFragment.java
@@ -1,7 +1,7 @@
 package com.example.shinjiung.guide_tour;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,43 +12,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.CameraUpdateFactory;
+
 
 /**
  * Created by shinjiung on 3/13/15.
  */
 public class TaipeiMapFragment extends Fragment {
-
-    GoogleMap mGoogleMap;
-    LatLng loc = new LatLng(25.046536,121.517532); // 위치 좌표 설정
-    CameraPosition cp = new CameraPosition.Builder().target((loc)).zoom(16).build();
-    MarkerOptions marker = new MarkerOptions().position(loc); // 구글맵에 기본마커 표시
-
-    private SupportMapFragment fragment;
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.taipei_map, container, false);
-    }
 
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        FragmentManager fm = getChildFragmentManager();
-        fragment = (SupportMapFragment) fm.findFragmentById(R.id.map);
-        if (fragment == null) {
-            fragment = SupportMapFragment.newInstance();
-            fm.beginTransaction().replace(R.id.map, fragment).commit();
-        }
     }
-    /*
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (mGoogleMap == null) {
-            mGoogleMap = fragment.getMap();
-            mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)));
-        }
-    }
-    */
 }
